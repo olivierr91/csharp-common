0793afc46d51ffb5a53cdc30f2ad356b65460efb
Olivier Robert <o.robert@hotmail.com>
2018-06-26 12:45:23 -0400

    Fixed bug in StringExtensions.Minus()

--- Utils/Text/StringUtils.cs
+++ Utils/Text/StringUtils.cs
@@ -5,7 +5,7 @@
             for (int i = 0; i < str2.Length; i++) {
                 var str2Part = str2.Substring(i);
                 if (str1.StartsWith(str2Part)) {
-                    return str1.Replace(str2Part, "");
+                    return str1.Remove(0, str2Part.Length);
                 }
             }
             return str1;
