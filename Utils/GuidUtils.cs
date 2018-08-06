6489b2333f78073d550253e4726cf61d3cd54b2f
Olivier Robert <o.robert@hotmail.com>
2018-08-06 15:12:12 -0400

    New ObjectUils.ConvertTo method + New GuidUtils.IsValidGuid method

---
+++ Utils/GuidUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoNameDev.CSharpCommon.Utils {
+
+    public static class GuidUtils {
+
+        public static bool IsValidGuid(string guidStr) {
+            Guid guid = default;
+            return Guid.TryParse(guidStr, out guid);
+        }
+    }
+}
--- Utils/ObjectUtils.cs
+++ Utils/ObjectUtils.cs
@@ -1,8 +1,31 @@
-﻿using System;
+﻿using NoNameDev.CSharpCommon.Extensions;
+using System;
 
 namespace NoNameDev.CSharpCommon.Utils {
 
     public static class ObjectUtils {
+
+        public static TValue ConvertTo<TValue>(object obj) {
+            if (typeof(TValue).IsNullable() && obj == null) {
+                return default;
+            } else if (typeof(TValue) == obj?.GetType()) {
+                return (TValue)obj;
+            } else if (typeof(TValue).IsEnum) {
+                return (TValue)(object)Convert.ToInt32(obj);
+            } else if (typeof(TValue).IsNullableEnum()) {
+                return (TValue)Enum.ToObject(Nullable.GetUnderlyingType(typeof(TValue)), Convert.ToInt32(obj));
+            } else if (typeof(TValue) == typeof(Guid)) {
+                return (TValue)(object)Guid.Parse(obj?.ToString());
+            } else if (typeof(TValue) == typeof(Guid?)) {
+                return (TValue)(object)(Guid?)Guid.Parse(obj?.ToString());
+            } else if (typeof(TValue).HasInterface<IConvertible>()) {
+                return (TValue)Convert.ChangeType(obj, typeof(TValue));
+            } else if (typeof(TValue).IsNullable() && Nullable.GetUnderlyingType(typeof(TValue)).HasInterface<IConvertible>()) {
+                return (TValue)Convert.ChangeType(obj, Nullable.GetUnderlyingType(typeof(TValue)));
+            } else {
+                return (TValue)obj;
+            }
+        }
 
         public static bool Equals<T>(T oldValue, T newValue, StringComparison stringComparisonType = StringComparison.InvariantCultureIgnoreCase) {
             if (typeof(T) == typeof(string)) {
